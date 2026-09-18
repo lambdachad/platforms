4c3ffdd0318873cd924ca1d917dfd9088bc0af73
Odd-Harald <oddharald@myhren.ai>
2026-09-18 10:46:12 +0300

    Error svelte pages

---
+++ desktop/src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from "$app/state"
+</script>
+
+<div class="error">
+    <p class="error-code">{page.status}</p>
+    <h1>Something broke</h1>
+    <a href="/">Go home</a>
+</div>
